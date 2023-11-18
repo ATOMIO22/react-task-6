111aa285d3fe4a487fafd7293bccba7adcec6648
Shaik Farhan <128593459+ATOMIO22@users.noreply.github.com>
2023-11-18 21:04:31 +0530

    commt

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,17 @@ function Home() {
     return (
         <div id="maindiv">
             <center>
-                <h1> I'm <span class="text-warning">SHAIK FARHAN</span></h1>
+                <h1> I'm <span class="text-warning">Bellamkonda Hazarath</span></h1>
                 <h1 class="text-primary"> Web Designer and Developer</h1>
                 <br></br>
                 <p>Aspiring and motivated fresher seeking an internship opportunity to gain valuable industry experience
                     and contribute my enthusiasm and knowledge to an organization. Eager to apply my skills and passion
                     to make a positive impact while fostering personal and professional growth.</p>
                 <br></br>
-                <a href="https://www.linkedin.com/in/farhan-shaik-90044323a/" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.linkedin.com/in/hajarath-bellamkonda-0716a3277/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">
                     <Linkedin size={35}  color = "black" className="rounded mx-4" />
                 </a>
-                <a href="https://www.instagram.com/farhan_2__/" target="_blank" rel="noopener noreferrer">
+                <a href="" target="_blank" rel="noopener noreferrer">
                     <Instagram size={35} color="black" className="rounded mx-4" />
                 </a>
                 <a href="https://github.com/ATOMIO22" target="_blank" rel="noopener noreferrer">
